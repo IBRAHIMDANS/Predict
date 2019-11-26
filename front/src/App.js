5193376336c1f04197e2411ca41c9f01cdb3c8ff
ibrahima <ibrahima.dansoko@outlook.com>
2019-11-26 14:51:18 +0100

    add front

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function App() {
+   /* const response = await axios.post(
+        'http://localhost:3000/predict',
+        {example: 'data'},
+        {headers: {'Content-Type': 'application/json'}}
+    )*/
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input type="text" value='ddd' />
+                <input value='SEND' type='button'/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
